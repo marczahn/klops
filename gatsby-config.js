b9be821329647da0eeaaa009173138b524512537
Marc Zahn <marc.zahn@loopline-systems.com>
2019-12-22 15:34:03 +0100

    Initial

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+/* eslint-disable */
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-less"
+  ]
+};
