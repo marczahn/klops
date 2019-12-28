c01597a7fb402244ea9c05ace5f3981d07d234fb
Marc Zahn <marc.zahn@loopline-systems.com>
2019-12-29 00:45:11 +0100

    Adjust readme; Add some custom blocks

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Klops is a small game which is inspired by Tetris (obviously). It is a playgroun
 
 At the moment it feels a little clunky because amongst rendering the matrix with a react component.
 
-The aim is a a multiplayer game with coop, "deathmatch" and "team deathmatch" - Or alone, of course. This is supposed to be implemented with websockets.
+There are these aims:
+
+* multiplayer with coop, "deathmatch" and "team deathmatch"
+* frontend based configuration of blocks
+* websockets
--- src/services/blocks.ts
+++ src/services/blocks.ts
@@ -18,9 +18,11 @@ import {vector} from './interfaces'
 
 const blockVectors: vector[][] = [
 	[{x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}, {x: 0, y: 3}],
-//	[{x: 0, y: 0}, {x: 1, y: 1}],
-//	[{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 0}, {x: 1, y: 1}],
-//	[{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}],
+	[{x: 0, y: 0}, {x: 2, y: 0}],
+	[{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 0}, {x: 1, y: 1}],
+	[{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}],
+	[{x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}],
+	[{x: 0, y: 1}, {x: 1, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}],
 ]
 
 export const getRandomBlockVector = (): vector[] => {
