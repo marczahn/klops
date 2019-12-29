cb55569c984d590260af20cd02ee11a30df2bf15
Marc Zahn <marc.zahn@loopline-systems.com>
2019-12-29 01:21:39 +0100

    Improved performance

--- src/modules/game/Game.tsx
+++ src/modules/game/Game.tsx
@@ -5,32 +5,6 @@ import {start} from '../../services/local'
 const Game: FC = () => {
 	const controls = start(10, 20)
 
-	const handleArrowPress = (e: KeyboardEvent) => {
-		switch (e.code) {
-			case 'ArrowRight':
-				controls.moveRight()
-				break
-			case 'ArrowLeft':
-				controls.moveLeft()
-				break
-			case 'ArrowDown':
-				controls.moveDown()
-				break
-			case 'ArrowUp':
-				controls.rotate()
-				break
-		}
-	}
-
-	useEffect(() => {
-		document.addEventListener('keydown', handleArrowPress)
-		return () => {
-			document.removeEventListener('keydown', handleArrowPress)
-			// When you switch the page
-			controls.stopGame()
-		}
-	})
-
 	return (<Matrix gameControls={controls}/>)
 }
 export default Game
--- src/modules/game/Matrix.tsx
+++ src/modules/game/Matrix.tsx
@@ -1,27 +1,61 @@
 import React, {FC, useEffect, useState} from 'react'
 import {MatrixTd} from './matrix.sc'
-import {GameControls, GameState} from '../../services/local'
+import {GameControls, ExternalGameState} from '../../services/interfaces'
 
 interface Props {
 	gameControls: GameControls;
 }
 
 const Matrix: FC<Props> = (props: Props) => {
-	const [gameState, setGameState] = useState<GameState>(props.gameControls.getGameState)
+	const [gameState, setGameState] = useState<ExternalGameState>(props.gameControls.getGameState)
 	// matrixScalar is necessary because setGameState does not realize a changed matrix as a new value
 	const [_, setMatrixScalar] = useState<string>('')
+	let lastMove = Date.now()
 	let clearHandle = -1
 
 	useEffect(() => {
-		clearHandle = window.setInterval(() => {
-			const state = props.gameControls.getGameState()
-			setGameState(state)
-			setMatrixScalar(serializeMatrix(state.matrix))
-		}, 100)
+		clearHandle = window.setInterval(update, 100)
 		return () => {
 			window.clearTimeout(clearHandle)
 		}
-	})
+	}, [])
+
+	const handleArrowPress = (e: KeyboardEvent) => {
+		if (Date.now() - lastMove < 50) {
+			return
+		}
+		lastMove = Date.now()
+		switch (e.code) {
+			case 'ArrowRight':
+				props.gameControls.moveRight()
+				update()
+				break
+			case 'ArrowLeft':
+				props.gameControls.moveLeft()
+				update()
+				break
+			case 'ArrowDown':
+				props.gameControls.moveDown()
+				break
+			case 'ArrowUp':
+				props.gameControls.rotate()
+				break
+		}
+	}
+
+	useEffect(() => {
+		document.addEventListener('keydown', handleArrowPress)
+		return () => {
+			document.removeEventListener('keydown', handleArrowPress)
+			// When you switch the page
+		}
+	}, [])
+
+	const update = () => {
+		const state = props.gameControls.getGameState()
+		setGameState(state)
+		setMatrixScalar(serializeMatrix(state.matrix))
+	}
 
 	const serializeMatrix = (matrix: number[][]): string => {
 		return Array.from(matrix).reduce(
@@ -32,7 +66,7 @@ const Matrix: FC<Props> = (props: Props) => {
 		<table>
 			<tbody>{
 				gameState.matrix.map(
-					(yv, y) => <tr key={y}>{yv.map((xv, x) => <MatrixTd key={`${y}-${x}`}
+					(yv: number[], y: number) => <tr key={y}>{yv.map((xv, x) => <MatrixTd key={`${y}-${x}`}
 																		className={`cell-${xv}`}>{xv}</MatrixTd>)}</tr>)
 			}</tbody>
 		</table>
