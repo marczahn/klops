cb2b88f7c7c27bd50b19dbb168f940c7e34b322a
Marc Zahn <marc.zahn@loopline-systems.com>
2019-12-30 01:11:02 +0100

    Implement origignal block shapes

--- src/services/blocks.ts
+++ src/services/blocks.ts
@@ -17,12 +17,21 @@ TODO - Add weights or simply add blockCount multiple times to increase their occ
 import {vector} from './interfaces'
 
 const blockVectors: vector[][] = [
+	// Original blocks:
+	// 1x4
 	[{x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}, {x: 0, y: 3}],
-	[{x: 0, y: 0}, {x: 2, y: 0}],
-	[{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 0}, {x: 1, y: 1}],
-	[{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}],
+	// half-t left
+	[{x: 0, y: 2}, {x: 1, y: 2}, {x: 1, y: 1}, {x: 1, y: 0}],
+	// half-t right
+	[{x: 1, y: 2}, {x: 0, y: 2}, {x: 0, y: 1}, {x: 0, y: 0}],
+	// square
 	[{x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}],
-	[{x: 0, y: 1}, {x: 1, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}],
+	// T
+	[{x: 0, y: 1}, {x: 1, y: 1}, {x: 1, y: 0}, {x: 2, y: 1}],
+	// first one of the strange ones
+	[{x: 0, y: 1}, {x: 1, y: 1}, {x: 1, y: 0}, {x: 2, y: 0}],
+	// second one of the strange ones
+	[{x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}, {x: 2, y: 1}],
 ]
 
 export const getRandomBlockVector = (): vector[] => {
